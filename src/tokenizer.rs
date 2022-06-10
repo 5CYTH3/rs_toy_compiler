869e9f4cb1fd8ee6eb22e04a8de4735864f4a1e6
Scythe <76437151+5CYTH3@users.noreply.github.com>
2022-06-10 09:01:08 +0000

    ..

--- src/tokenizer.rs
+++ src/tokenizer.rs
@@ -1,7 +1,7 @@
 pub enum ExprType {
-    BinaryExpression { op: Operator, left: i64, right: i64 },
+    BinaryExpression { op: Operator, left: Box<Expr>, right: Box<Expr> },
     NumericLiteral { val: i64 },
-    AssignExpression { op: Operator, left: Identifier(String), right: i64 }
+    AssignExpression { op: Operator, left: Identifier(String), right: Box<Expr> }
 }
 
 // https://youtu.be/Ra_Fk7JFMoo?t=6012
@@ -29,18 +29,25 @@ pub struct Expr {
     pub expr_type: ExprType,
 }
 
+// TODO: Instead of unpacking each Expr, why not returning a final expr and get the final value of it with a function?
+// TODO: Use YOUR type instead of primitive types. Returning i64 is dumb as hell.
+// TODO: 
 impl Expr {
-    pub fn new(expr_type: ExprType) -> i64 {
+    pub fn new(expr_type: ExprType) -> Expr {
         match expr_type {
             ExprType::BinaryExpression { op, left, right } => match op {
-                Operator::PLUS => return left + right,
-                Operator::MIN => return left - right,
-                Operator::MUL => return left * right,
-                Operator::DIV => return left / right,
+                Operator::PLUS => return ExprType::NumericLiteral { val: left + right },
+                Operator::MIN => return ExprType::NumericLiteral { left - right },
+                Operator::MUL => return ExprType::NumericLiteral { left * right },
+                Operator::DIV => return ExprType::NumericLiteral { left / right },
                 Operator::EQUAL => todo!(),
             },
-            ExprType::NumericLiteral { val } => return val,
+            ExprType::NumericLiteral { val } => return ExprType::NumericLiteral { val },
             _ => return 0,
         }
+    }
+
+    pub fn to_int(&self) -> i64 {
+        
     }
 }
